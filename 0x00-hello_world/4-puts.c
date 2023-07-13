41c4f1b9f9cb6df35fb3792b29c030a7a8d64fc6
kyaloeriq <kyaloeriq@gmail.com>
2023-07-13 14:51:55 -0700

    Program that prints string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,13 @@
 /**
  * main - prints "Programming is like building a multilingual puzzle"
  * followed by new line.
- * return: Always 0.
+ * Return: Always 0.
  */
 
 int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
+	
 	return (0);
 }
