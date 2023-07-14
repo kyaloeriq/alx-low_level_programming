576bc8b657d43ef0b84b228f1f9f70a548e96e19
kyaloeriq <kyaloeriq@gmail.com>
2023-07-14 00:09:34 -0700

    prints and that piece of art is useful" - Dora Korpar, 2015-10-19, to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,8 @@
 #inside <stdio.h>
+#inside <unistd.h>
 
 /**
- * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19" to the standard error
  * followed by a new line.
  * Return: 1
  */
@@ -10,7 +11,7 @@ int main(void)
 
 {
 
-	printf("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	printf(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 
 }
