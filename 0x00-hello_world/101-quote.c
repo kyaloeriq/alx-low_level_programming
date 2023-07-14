7f6b330db527d742760df8a83f9fca913af47d6b
kyaloeriq <kyaloeriq@gmail.com>
2023-07-14 00:35:51 -0700

    prints and that piece of art is useful" - Dora Korpar, 2015-10-19, to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,16 @@
 /**
  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19" to the standard error
  * followed by a new line.
- * Return: 1
+ * Return: Always 1
  */
 
 int main(void)
 
 {
 
-	write(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	write(2,
+	"and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 
+	59);
 	return (1);
 
 }
