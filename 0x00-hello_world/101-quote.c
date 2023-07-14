3ad2b16f2de944ba5abd96913b65e7be81d11246
kyaloeriq <kyaloeriq@gmail.com>
2023-07-13 23:48:03 -0700

    Prints and that piece of art is useful" - Dora Korpar, 2015-10-19

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#inside <stdio.h>
+
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ * followed by a new line.
+ * Return: 1
+ */
+
+int main(void)
+
+{
+
+	printf("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	return (1);
+
+}
