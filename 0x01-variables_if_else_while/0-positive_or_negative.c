853b903b2394271742e5686f0e76e6e7ce527c00
kyaloeriq <kyaloeriq@gmail.com>
2023-07-14 14:19:43 -0700

    Prints the size of various type on the computer

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * assign a random number to the variable n each time it is executed
  * variable will store a different number every time program is run
- * Return: Always 0
+ * Return: Always (Success)
  */
 
 int main(void)
@@ -14,7 +14,11 @@ int main(void)
 	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
+	printf("%d \n", n);
+
 	/* print whether the number stored in variable n is positive or negative*/
+
 	if (n > 0)
 	{
 		printf("The number is positive \n");
