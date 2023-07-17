0a310b4fefa17e413041a3a945c2eddf820fe848
kyaloeriq <kyaloeriq@gmail.com>
2023-07-17 02:29:47 -0700

    prints all possible different combinations of three digits and all three must be different

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,16 +10,16 @@ int main(void)
 {
 	int a, b, c;
 
-	for (a = 0; a < 9; a++)
+	for (a = 0; a < 8; a++)
 	{
-		for (b = a + 1; b < 10; b++)
+		for (b = a + 1; b < 9; b++)
 		{
-			for (c = b + 1; c < 11; c++)
+			for (c = b + 1; c < 10; c++)
 			{
 				putchar((a % 10) + '0');
 				putchar((b % 10) + '0');
 				putchar((c % 10) + '0');
-				if (a == 8 && b == 9 && c == 10)
+				if (a == 7 && b == 8 && c == 9)
 				continue;
 			putchar(',');
 			putchar(' ');
