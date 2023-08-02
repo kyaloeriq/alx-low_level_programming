e8df6732a1e56d7c5d41285ecdc51264fd4a6519
kyaloeriq <kyaloeriq@gmail.com>
2023-08-02 05:42:12 -0700

    prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,16 +2,14 @@
 /**
  * _puts_recursion - prints a string
  * @s: String to be printed
- * Return: 0
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (s[0] != '\0')
 	{
-		_putchar(*s);
+		_putchar(s[0]);
 		_puts_recursion(s + 1);
 	}
 	else
 		_putchar('\n');
-	return(0);
 }
