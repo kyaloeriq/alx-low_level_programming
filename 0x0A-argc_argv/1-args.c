57fd83f19b157f8c3e97b249b81c3a6a78e490cd
kyaloeriq <kyaloeriq@gmail.com>
2023-08-05 01:04:04 -0700

    prints the number of arguments passed into it

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,10 +1,9 @@
-#include "main.h"
 #include <stdio.h>
 /**
  * main - prints the number of arguments passed
  * followed by a new line
  * @argc: number of arguments passed
- * @argv: array of strings containing arguments
+ * @argv: arrays of strings containing argumnts
  * Return: 0
  */
 int main(int argc, char *argv[])
