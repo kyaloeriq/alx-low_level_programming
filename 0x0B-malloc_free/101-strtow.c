743765eaa88f3c47c836704d37cc672581302ab9
kyaloeriq <kyaloeriq@gmail.com>
2023-08-09 07:51:06 -0700

    splits a string into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,16 +8,14 @@
  */
 char **strtow(char *str)
 {
-	int a, word_count, word_index;
+	int a, b, word_count, word_index;
 	char **word_array, *token;
 
 	word_count = 0;
-
 	if (str == NULL || *str == '\0')
 	{
 		return (NULL);
 	}
-
 	for (a = 0; str[a] != '\0'; a++)
 	{
 
@@ -27,13 +25,24 @@ char **strtow(char *str)
 		}
 	}
 	word_array  = (char **)malloc((word_count + 1) * sizeof(char *));
-
+	if (word_array == NULL)
+	{
+		return (NULL);
+	}
 	word_index = 0;
 	token = strtok(str, " ");
-
 	while (token != NULL)
 	{
-		printf("%s\n", token);
+		word_array[word_index] = strdup(token);
+		if (word_array[word_index] == NULL)
+		{
+			for (b = 0; b < word_index; b++)
+			{
+				free(word_array[b]);
+			}
+			free(word_array);
+			return (NULL);
+		}
 		word_index++;
 		token = strtok(NULL, " ");
 	}
