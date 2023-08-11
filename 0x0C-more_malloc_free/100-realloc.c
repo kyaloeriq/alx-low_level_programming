21e2f6629a60248fff91e379ba0f44af28810abf
kyaloeriq <kyaloeriq@gmail.com>
2023-08-11 03:58:11 -0700

    reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,7 +8,7 @@
  * @ptr: pointer to old mem allocation
  * Return: pointer to new mem allocation
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+void *_realloc(void *ptr, unsigned int new_size, unsigned int old_size)
 {
 	void *ptr1;
 
