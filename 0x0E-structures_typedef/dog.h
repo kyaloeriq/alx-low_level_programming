7370ae685cedcfef037ae7e0eb0b72f932163b24
kyaloeriq <kyaloeriq@gmail.com>
2023-08-14 09:06:25 -0700

    new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new type
+ * @name: 1st member
+ * @age: 2nd member
+ * @owner: 3rd member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
