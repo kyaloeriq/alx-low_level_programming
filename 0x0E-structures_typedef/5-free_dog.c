0dd193afeba65b1328881fa9f6be08f63dbb27fe
kyaloeriq <kyaloeriq@gmail.com>
2023-08-15 00:04:45 -0700

    function that frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_dog - function that frees dogs
+ * @d: param
+ * Return: 0
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		free(d);
+	}
+}
