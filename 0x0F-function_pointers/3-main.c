2c0a8959ff6917d766583e285ca9e6f0df15e94c
kyaloeriq <kyaloeriq@gmail.com>
2023-08-16 15:15:14 -0700

    program that performs simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * main - check the code
+ */
+int main(int argc, char *argv[])
+{
+	char operator;
+	int num1, num2;
+	int result;
+	int (*calc_func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+       
+	operator = argv[2][0];
+
+	if (operator != '+' && operator != '-' && operator != '*' && operator != '/' && operator != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((operator == '/' || operator == '%') && atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+       
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	calc_func = get_op_func(&operator);
+
+	if (calc_func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	
+       	result = calc_func(num1, num2);
+	printf("%d\n", result);
+	return (0);
+}
+
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ * op_add - returns the sum of a and b
+ * @a: 1st param
+ * @b: 2nd param
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - returns the difference of a and b
+ * @a: 1st param
+ * @b: 2nd param
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - returns the product of a and b
+ * @a: 1st param
+ * @b: 2nd param
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - returns the division of a by b
+ * @a: 1st param
+ * @b: 2nd param
+ * Return: division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - returns the remainder of the division of a by b
+ * @a: 1st param
+ * @b: 2nd param
+ * Return: modulo
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
