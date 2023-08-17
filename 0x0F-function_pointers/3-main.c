c77f9c51fbe4e659dd3e629b95eb7d415a7633ff
kyaloeriq <kyaloeriq@gmail.com>
2023-08-17 01:52:17 -0700

    program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -24,8 +24,12 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-
 	if (calc_func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (op != '+' && op != '-' && op != '*' && op != '/' && op != '%')
 	{
 		printf("Error\n");
 		exit(99);
