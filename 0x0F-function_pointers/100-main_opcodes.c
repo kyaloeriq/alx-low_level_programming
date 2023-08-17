1ee02fd4ce7f9e9c36640583456c11e3562a58e2
kyaloeriq <kyaloeriq@gmail.com>
2023-08-17 15:13:59 -0700

    program that prints the opcodes of its own main function

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: number of command-line arguments
+ * @argv: array of command line arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int number_of_bytes;
+	unsigned char *main_ptr;
+	int a;
+
+	number_of_bytes = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	number_of_bytes = atoi(argv[1]);
+
+	if (number_of_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	printf("%d\n", number_of_bytes);
+
+	main_ptr = (unsigned char *)main;
+
+	for (a = 0; a < number_of_bytes; a++)
+	{
+		printf("%02x ", main_ptr[a]);
+	}
+	printf("\n");
+	return (0);
+}
+
