4329ca327a3c1e7d927fe8020a8c666046ee454c
kyaloeriq <kyaloeriq@gmail.com>
2023-08-21 01:49:51 -0700

    function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -17,22 +17,25 @@ void print_all(const char * const format, ...)
 	va_start(args, format);
 	while (format && format[a])
 	{
+		while (format[a] != 'c' && format[a] != 'i' && format[a] != 'f' && format[a] != 's' && format[a])
+		{
+			a++;
+		}
+		if (!format[a])
+			break;
 		if (format[a] == 'c')
 		{
 			printf("%s%c", separator, va_arg(args, int));
-			separator = ", ";
 		}
-		else if (format[a] == 'i')
+		if (format[a] == 'i')
 		{
 			printf("%s%d", separator, va_arg(args, int));
-			separator = ", ";
 		}
-		else if (format[a] == 'f')
+		if (format[a] == 'f')
 		{
 			printf("%s%f", separator, va_arg(args, double));
-			separator = ", ";
 		}
-		else if (format[a] == 's')
+		if (format[a] == 's')
 		{
 
 			char *str = va_arg(args, char *);
@@ -41,8 +44,8 @@ void print_all(const char * const format, ...)
 				printf("(%s(nil)", separator);
 			else
 				printf("%s%s", separator, str);
-			separator = ", ";
 		}
+		separator = ", ";
 		a++;
 	}
 	va_end(args);
