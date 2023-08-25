06586bb4c41479da10bbefa00e4600c2848a838b
kyaloeriq <kyaloeriq@gmail.com>
2023-08-25 04:20:11 -0700

    function that prints str before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_str - prints string before the main function is executed
+ * Return: str
+ */
+void print_str(void)
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
