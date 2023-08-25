a930588351261b7eec6dceb58b6b9e4d3c5c994f
kyaloeriq <kyaloeriq@gmail.com>
2023-08-25 04:32:50 -0700

    function that prints str before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,5 +7,6 @@
 void print_str(void) __attribute((constructor));
 void print_str(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
