c638cc69fc90de591fe4169bfa0a0cdc0dd02e3b
kyaloeriq <kyaloeriq@gmail.com>
2023-08-29 09:19:38 -0700

    reverses a listint_t linked list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -11,6 +11,11 @@ listint_t *reverse_listint(listint_t **head)
 	listint_t *prev = NULL;
 	listint_t *current = *head;
 
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return (*head);
+	}
+
 	while (current != NULL)
 	{
 		listint_t *next = current->next;
