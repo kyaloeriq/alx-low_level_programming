bf729b37c8ff6ddfbcfe3118a934af13ccec28b4
kyaloeriq <kyaloeriq@gmail.com>
2023-09-05 09:32:56 -0700

    displays the information contained in the ELF header at the start of an ELF file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include "main.h"
 /**
  * main - displays info. contained in the ELF header
  * at the start of an ELF file
@@ -10,7 +12,7 @@
 int main(int argc, char *argv[])
 {
 	FILE *efile;
-	unsigned char magic[4];
+	ELFhdr a;
 
 	if (argc != 2)
 	{
@@ -23,17 +25,23 @@ int main(int argc, char *argv[])
 		perror("There might be an error\n");
 		exit(98);
 	}
-	fread(magic, 1, 4, efile);
-	if (magic[0] == 0x7f && magic[1] == 'E' && magic[2] == 'L' && magic[3] == 'F')
-	{
-		printf("This is an ELF file.\n");
-	}
-	else
+	fread(&a, sizeof(a), 1, efile);
+	if (a.e_ident[0] != 0x7f || a.e_ident[1] != 'E' || a.e_ident[2] != 'L' || a.e_ident[3] != 'F')
 	{
 		printf("This is not an ELF file.\n");
 		fclose(efile);
 		exit(98);
 	}
+
+	printf("Magic: %02x %02x %02x %02x\n", a.e_ident[0], a.e_ident[1], a.e_ident[2], a.e_ident[3]);
+	printf("Class: %d-bit\n", (a.e_ident[4] == 1) ? 32 : 64);
+	printf("Data: %s-endian\n", (a.e_ident[5] == 1) ? "Little" : "Big");
+	printf("Version: %d\n", a.e_ident[6]);
+	printf("OS/ABI: %d\n", a.e_ident[7]);
+	printf("ABI Version: %d\n", a.e_ident[8]);
+	printf("Type: %d\n", a.e_type);
+	printf("Entry point address: 0x%x\n", a.e_entry);
+
 	fclose(efile);
 	return (0);
 }
