d748675478de9a738663cd408da15410a29f291a
kyaloeriq <kyaloeriq@gmail.com>
2023-11-28 14:29:04 -0800

    copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,67 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include "main.h"
-/**
- * close_and_exit - Closes file descriptor
- * @fd: File descriptor to close
- * @code_exit: Exit code incase of error
- */
-void close_and_exit(int fd, int code_exit, const char *file_name)
+
+void close_and_exit(int fd, int exit_code) 
 {
-	if (close(fd) == -1)
+	if (close(fd) == -1) 
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd);
-		exit(code_exit);
+		fprintf(stderr, "Error: Can't close fd %d\n", fd);
+		perror("close");
+		exit(exit_code);
 	}
 }
-/**
- * main - copies the content of a file to another file
- * @argc: 1st argument
- * @argv: 2nd argument
- * Return: 0
- */
 int main(int argc, char *argv[])
 {
 	const char *file_from = argv[1];
 	const char *file_to = argv[2];
-	int from, to;
+	FILE *from, *to;
 	char buffer[1024];
-	size_t bytes_read;
+	size_t read_bytes;
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		fprintf(stderr, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
-	from = open(file_from, O_RDONLY);
-	if (from == -1)
+	from = fopen(file_from, "rb");
+	if (from == NULL)
 	{
-		dprintf(2, "Error: Can't read from %s\n", file_from);
+		perror("Can't read from source file");
 		exit(98);
 	}
-	/*O_CREAT with mode 0644 and O_TRUNC to create/truncate the file*/
-	to = open(file_to, O_WRONLY | O_CREAT, O_TRUNC, 0644);
-	if (to == -1)
+	to = fopen(file_to, "wb");
+	if (to == NULL)
 	{
-		dprintf(2, "Error: Can't write to %s\n", file_to);
-		close_and_exit(from, 99, file_from);
+		perror("Can't write to destination file");
+		close_and_exit(fileno(from), 100);
+		exit(99);
 	}
-	while ((bytes_read = read(from, buffer, sizeof(buffer))) > 0)
+	while ((read_bytes = fread(buffer, 1, sizeof(buffer), from)) > 0)
 	{
-		if (write(to, buffer, bytes_read) != bytes_read)
+		if (fwrite(buffer, 1, read_bytes, to) != read_bytes)
 		{
-			dprintf(2, "Error: Can't write to %s\n", file_to);
-			close_and_exit(from, 99, file_from);
+			perror("Can't write to destination file");
+			close_and_exit(fileno(from), 100);
+			close_and_exit(fileno(to), 100);
+			exit(100);
 		}
 	}
-	if (close(to) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", to);
-		exit(100);
-	}
-	close_and_exit(from, 0, file_from);
+	close_and_exit(fileno(from), 100);
+	close_and_exit(fileno(to), 100);
+	return (0);
 }
