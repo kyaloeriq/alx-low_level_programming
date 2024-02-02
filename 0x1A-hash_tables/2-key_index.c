8c26972c024d7d8eeead24abfb474c51b2b6a9b4
kyaloeriq <kyaloeriq@gmail.com>
2024-02-02 14:35:39 -0800

    function that gives you the index of a key

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -6,20 +6,19 @@
  *key_index - gives you the index of a key
  *@key: key
  *@size: size of the array of the hash table
- *Return:  index at which the key/value pair should be stored 
+ *Return: index at which the key/value pair should be stored
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-        unsigned long int hash;
-        int c;
+	unsigned long int hash;
+	int c;
 
-        hash = 5381;
-        while ((c = *key++))
-        {
-                hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-        }
+	hash = 5381;
+	while ((c = *key++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
 	/*Reduce the hash value to fit within the table size*/
 	hash %= size;
-
-        return (hash);
+	return (hash);
 }
