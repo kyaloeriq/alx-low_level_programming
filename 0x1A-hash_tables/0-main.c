d0468ea94867a862be7ae90c9f9a12cb98c46530
kyaloeriq <kyaloeriq@gmail.com>
2024-02-02 13:50:39 -0800

    function that creates a hash table, main.c file

--- 0x1A-hash_tables/0-main.c
+++ 0x1A-hash_tables/0-main.c
@@ -10,9 +10,9 @@
  */
 int main(void)
 {
-    hash_table_t *ht;
-
-    ht = hash_table_create(1024);
-    printf("%p\n", (void *)ht);
-    return (EXIT_SUCCESS);
+	hash_table_t *ht;
+	
+	ht = hash_table_create(1024);
+	printf("%p\n", (void *)ht);
+	return (EXIT_SUCCESS);
 }
