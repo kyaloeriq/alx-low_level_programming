0875bba9bcea8f26765b0f9da2df780af22c8794
kyaloeriq <kyaloeriq@gmail.com>
2024-02-02 15:18:37 -0800

    function that adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -6,7 +6,8 @@
  *hash_table_set - adds an element to the hash table
  *@ht: hash table you want to add or update the key/value to
  *@key: key
- *@value:  value associated with the key
+ *@value: value associated with the key
+ *Return: 1 if it succeeded, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
@@ -21,29 +22,30 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	/*Check if the key already exists in the hash table*/
 	temp = ht->array[index];
-	while (temp != NULL) {
-		if (strcmp(temp->key, key) == 0) {
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
 			free(temp->value);
 			temp->value = strdup(value); /*Update the value*/
-			return (1);
-		}
-		temp = temp->next;
-	}
+			return (1); }
+		temp = temp->next; }
 	/*If key doesn't exist, create a new node*/
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0); /*Allocation failed*/
 	new_node->key = strdup(key);
-	if (new_node->key == NULL) {
+	if (new_node->key == NULL)
+	{
 		free(new_node);
-		return (0); /*Allocation failed*/
-	}
+		return (0); /*Allocation failed*/ }
 	new_node->value = strdup(value);
-	if (new_node->value == NULL) {
+	if (new_node->value == NULL)
+	{
 		free(new_node->key);
 		free(new_node);
-		return (0); /*Allocation failed*/
-	}/*Insert the new node at the beginning of the linked list at index*/
+		return (0); /*Allocation failed*/ }
+	/*Insert the new node at the beginning of the linked list at index*/
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
