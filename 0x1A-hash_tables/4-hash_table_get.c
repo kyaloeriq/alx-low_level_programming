1fac83a4c2326eaee142826367044d5ac1ffbae2
kyaloeriq <kyaloeriq@gmail.com>
2024-02-06 23:22:02 -0800

    retrieves a value associated with a key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: hash table you want to look into
+ * @key: key you are looking for
+ * Return: value associated with the element, NULL if key couldn’t be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	*key = *value;
+
+	if (key == NULL)
+		return (NULL);
+	else
+		return (value);
