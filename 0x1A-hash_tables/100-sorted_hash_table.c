ffeafee3b9323f4a64dc183970ab60d99ecab41a
kyaloeriq <kyaloeriq@gmail.com>
2024-02-07 06:43:16 -0800

    sorted_hash_table

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -37,7 +37,7 @@ shash_table_t *shash_table_create(unsigned long int size)
 int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 {
         shash_node_t *new_node = NULL;
-        shash_node_t *temp;
+        shash_node_t *temp, *prev = NULL;
         unsigned long int index;
 
         if (ht == NULL || key == NULL || value == NULL)
@@ -45,18 +45,21 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 
         index = hash_djb2((const unsigned char *)key) % ht->size;
 
+	temp = ht->array[index];
+	while (temp != NULL && strcmp(temp->key, key) < 0)
+	{
+		prev = temp;
+		temp = temp->next;
+	}
+
         /*Check if the key already exists in the hash table*/
-        temp = ht->array[index];
-        while (temp != NULL)
-        {
-                if (strcmp(temp->key, key) == 0)
-                {
-                        free(temp->value);
-                        temp->value = strdup(value); /*Update the value*/
-			if (temp->value == NULL)
-				return (0); /*Memory allocation failed*/
-                        return (1); }
-                temp = temp->next; }
+	if (temp != NULL && strcmp(temp->key, key) == 0)
+	{
+		free(temp->value);
+		temp->value = strdup(value); /* Update the value */
+		return (1);
+	}
+
         /*If key doesn't exist, create a new node*/
         new_node = malloc(sizeof(shash_node_t));
         if (new_node == NULL)
@@ -73,8 +76,16 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
                 free(new_node);
                 return (0); /*Allocation failed*/ }
         /*Insert the new node at the beginning of the linked list at index*/
-        new_node->next = ht->array[index];
-        ht->array[index] = new_node;
+        if (prev == NULL)
+	{
+		new_node->next = ht->array[index];
+		ht->array[index] = new_node;
+	}
+	else
+	{
+		new_node->next = prev->next;
+		prev->next = new_node;
+	}
         return (1);
 }
 /**
