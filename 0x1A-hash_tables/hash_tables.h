071776d491b8f4b18f166eec44332b51642b394e
kyaloeriq <kyaloeriq@gmail.com>
2024-02-07 05:22:16 -0800

    header file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -75,5 +75,6 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 shash_table_t *shash_table_create(unsigned long int size);
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 
 #endif
