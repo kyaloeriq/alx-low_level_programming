6144cf717a78940f238809bc4581609af1a12e71
kyaloeriq <kyaloeriq@gmail.com>
2024-02-07 01:09:47 -0800

    function that prints a hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,8 +8,25 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	unsigned long int j;
+	hash_node_t *node;
+	int flag = 0;
+
 	if (ht != NULL)
 	{
-		print(ht);
+		printf("{");
+		for (j = 0; j < ht->size; j++)
+		{
+			node = ht->array[j];
+			while (node != NULL)
+			{
+				if (flag == 1)
+					printf(", ");
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				flag = 1;
+			}
+		}
+		printf("}\n");
 	}
 }
