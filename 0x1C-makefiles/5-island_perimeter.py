4692e258273583ce29ff6c0323c11f83c6a6c2ab
kyaloeriq <kyaloeriq@gmail.com>
2024-03-20 11:43:07 -0700

    function returns the perimeter of the island

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of the island described in grid.
+    Args:
+    grid (list of list of int): Grid representation where 0 represents water and 1 represents land.
+    Returns:
+    int: Island perimeter.
+    """
+    prmter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                prmter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    prmter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    prmter -= 2
+
+    return prmter
+
+
+
+
+
+
